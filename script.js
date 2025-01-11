f5a631ceae52c3ae0507a902b498df68ebfeca30
MshTsh <188133929+Mshprog@users.noreply.github.com>
2025-01-11 20:22:26 +0800

    Add random number generation (1-3) and string assignment

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 function getComputerChoice() {
-  let randomNum;
+  let max = 4;
+  let min = 1;
+
+  let generateRandomNum = Math.floor(Math.random() * (max - min) + 1);
+
+  if (generateRandomNum === 1) {
+    return alert("Computer Chose: ROCK");
+  }
+  if (generateRandomNum === 2) {
+    return alert("Computer Chose: PAPER");
+  }
+  if (generateRandomNum === 3) {
+    return alert("Computer Chose: SCISSORS");
+  }
 }
+
+function getHumanChoice() {
+  let humanInput;
+
+  while (
+    humanInput != "ROCK" &&
+    humanInput != "PAPER" &&
+    humanInput != "SCISSORS" &&
+    humanInput != null
+  ) {
+    humanInput = prompt(
+      "Choose between ROCK, PAPER, or SCISSORS"
+    ).toUpperCase();
+  }
+
+  return alert("You Chose : " + humanInput);
+}
+
+console.log(getHumanChoice);
+
+getHumanChoice();
+
+// function playRound() {}
+
+// function playGame() {}
