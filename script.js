0b3ddc7d9814180d3f067b9f97fbb9af0daa611b
MshTsh <188133929+Mshprog@users.noreply.github.com>
2025-01-15 15:51:01 +0800

    Finish adding logic to the playRound() function to check computer and human input to increment score depending on who the winner is

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let humanScore = 0; //For storing player score
+let computerScore = 0; // For storing computer score
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
 function getComputerChoice() {
   let max = 4;
   let min = 1;
@@ -5,13 +11,16 @@ function getComputerChoice() {
   let generateRandomNum = Math.floor(Math.random() * (max - min) + 1);
 
   if (generateRandomNum === 1) {
-    return alert("Computer Chose: ROCK");
+    alert("Computer Chose: ROCK");
+    return "ROCK";
   }
   if (generateRandomNum === 2) {
-    return alert("Computer Chose: PAPER");
+    alert("Computer Chose: PAPER");
+    return "PAPER";
   }
   if (generateRandomNum === 3) {
-    return alert("Computer Chose: SCISSORS");
+    alert("Computer Chose: SCISSORS");
+    return "SCISSORS";
   }
 }
 
@@ -23,12 +32,63 @@ function getHumanChoice() {
     humanInput != "PAPER" &&
     humanInput != "SCISSORS"
   ) {
-    humanInput = prompt("Choose between ROCK, PAPER, or SCISSORS");
-  }
+    humanInput = prompt(
+      "Choose between ROCK, PAPER, or SCISSORS"
+    ).toUpperCase();
 
-  return alert("You Chose : " + humanInput);
+    if (humanInput === null) {
+      return;
+    }
+  }
+  alert("You Chose : " + humanInput);
+  return humanInput;
 }
 
-function playRound() {}
+function playRound(humanChoice, computerChoice) {
+  switch (humanChoice) {
+    case "ROCK":
+      if (computerChoice === "ROCK") {
+        alert("It's a draw!");
+      } else if (computerChoice === "PAPER") {
+        alert("You lose! Paper beats Rock");
+        computerScore++;
+        alert("Player:" + humanScore + " " + " Computer:" + computerScore);
+      } else if (computerChoice === "SCISSORS") {
+        alert("You win! Rock beats Scissors");
+        humanScore++;
+        alert("Player:" + humanScore + " " + " Computer:" + computerScore);
+      }
+      break;
+
+    case "PAPER":
+      if (computerChoice === "PAPER") {
+        alert("It's a draw!");
+      } else if (computerChoice === "SCISSORS") {
+        alert("You lose! Scissors beats Paper ");
+        computerScore++;
+        alert("Player:" + humanScore + " " + " Computer:" + computerScore);
+      } else if (computerChoice === "ROCK") {
+        alert("You win! Paper beats Rock");
+        humanScore++;
+        alert("Player:" + humanScore + " " + " Computer:" + computerScore);
+      }
+      break;
+    case "SCISSORS":
+      if (computerChoice === "SCISSORS") {
+        alert("It's a draw!");
+      } else if (computerChoice === "ROCK") {
+        alert("You lose! Rock beats Scissors");
+        alert("Player:" + humanScore + " " + " Computer:" + computerScore);
+        return computerScore++;
+      } else if (computerChoice === "PAPER") {
+        alert("You win! Scissors beats Paper");
+        humanScore++;
+        alert("Player:" + humanScore + " " + " Computer:" + computerScore);
+      }
+      break;
+  }
+}
+
+playRound(humanSelection, computerSelection);
 
 // function playGame() {}
